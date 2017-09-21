775133ceded383b563652e48e3f44aa2cc23e641
Colleen Yu <colleenyu98@berkeley.edu>
2017-09-20 23:19:00 -0700

    hw1

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,9 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    b = a.map{ |s| s.to_i + 2 }.map{ |s| s.to_i if s % 2 == 0 && s <= 10}.uniq
+      # Keep only these elements / reject these elements: a.select
+
+    return b.sum
   end
 end
-
-
